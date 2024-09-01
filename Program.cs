96086499216ed9a6fbfce990544a962f8223c04c
Sophie Gabriela Laksø <slak@itu.dk>
2024-09-01 17:31:27 +0200

    Can read, print, add Cheeps
    Program can read, print and add Cheeps
    Needed:
    -format original Cheeps including proper timestamp
    -add author+timestamp to new Cheeps

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+StreamReader input = new("chirp_cli_db.csv");
+
+List<string> cheeps = [];
+var nextLine = input.ReadLine();
+while ((nextLine = input.ReadLine()) != null) {
+   cheeps.Add(nextLine);
+}
+
+if (args[0]=="read") {
+    foreach (var cheep in cheeps) {
+        Console.WriteLine(cheep);
+        Thread.Sleep(1000);
+    }
+} else if (args[0]=="cheep"){
+    //var authorname
+    var cheepString = String.Join(" ", args[1..]);
+    //var timestamp
+    //join authorname, cheepString and timestamp
+    cheeps.Add(cheepString);
+    foreach (var cheep in cheeps) {
+        Console.WriteLine(cheep);
+        Thread.Sleep(1000);
+    }
+}
