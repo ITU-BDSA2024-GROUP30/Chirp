704bcaf81c34b00964a7f9418093aefb8bf7f0b8
HenrijetteOest <hoes@itu.dk>
2024-10-25 15:52:58 +0200

    updated EF Core folder structure Co-authored-by: Henriette <hoes@itu.dk>

--- EFCore/AppDBContext.cs
+++ EFCore/AppDBContext.cs
@@ -6,7 +6,7 @@ public class AppDBContext : DbContext {
     public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) {}
     public AppDBContext() {}
     
-    public DbSet<Cheep> Cheeps { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
-    public DbSet<Author> Authors { get; set; }
+    public DbSet<User> Users { get; set; }
 }
