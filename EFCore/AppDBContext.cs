a54046045ba2e754a9c46d146a246af322849ddf
HenrijetteOest <hoes@itu.dk>
2024-10-25 15:52:58 +0200

    updated EF Core folder structure Co-authored-by: Henriette <hoes@itu.dk>

--- EFCore/AppDBContext.cs
+++ EFCore/AppDBContext.cs
@@ -5,7 +5,7 @@ namespace Chirp.EFCore;
 public class AppDBContext : DbContext {
     public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) {}
     
-    public DbSet<Cheep> Cheeps { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
-    public DbSet<Author> Authors { get; set; }
+    public DbSet<User> Users { get; set; }
 }
