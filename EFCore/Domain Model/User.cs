3d2b2b191f3205033dd1383fb22c100f522f75f4
Green-Peber <114397502+HenrijetteOest@users.noreply.github.com>
2024-11-01 15:07:52 +0100

    Update User.cs
    updated User with small fix on values names.
    Co-authored-by: Henriette <hoes@itu.dk>

--- EFCore/Domain Model/User.cs
+++ EFCore/Domain Model/User.cs
@@ -5,8 +5,8 @@ namespace Chirp.EFCore;
 //[PrimaryKey(nameof(user_id))]
 public class User
 {
-    public required int user_id { get; set; }
-    public required string user_name { get; set; }
+    public required int UserId { get; set; }
+    public required string Name { get; set; }
 
     public required ICollection<Message> MessageProcessingHandler { get; set; }
     //public required string email { get; set; }
