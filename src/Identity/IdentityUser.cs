64327db8443d01f88f60b942fdbf9f5f504ded7e
henri <hoes@itu.dk>
2024-11-07 12:53:13 +0100

    deleted  unnessesary things Co-authored-by: Henriette <hoes@itu.dk>

--- src/Identity/IdentityUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Chirp.Identity;
-public class IdentityUser
-{
-	public required string name { get; set; }
-	public required string email { get; set; }
-
-}
