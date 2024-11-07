d837514080923843a36ae49575ba0449d1436807
henri <hoes@itu.dk>
2024-11-07 13:14:02 +0100

    Updated Cheep
    Co-authored-by: Henriette <hoes@itu.dk>

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -6,7 +6,6 @@ public class Author : IdentityUser
 {
 	public int AuthorId { get; set; }
 	public string? Name { get; set; }
-	public string Email { get; set; }
-
+	public string? Email { get; set; }
 	public required ICollection<Cheep> Cheeps { get; set; }
 }
