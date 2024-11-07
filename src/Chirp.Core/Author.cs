2c16b5cff81888c2d58f08dee48c0b1016c32201
henri <hoes@itu.dk>
2024-11-07 13:10:30 +0100

    Refactored Author
    Co-authored-by: Henriette <hoes@itu.dk>

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -4,7 +4,9 @@ namespace Chirp.EFCore;
 
 public class Author : IdentityUser
 {
-	public string Name { get; set; }
-    public int AuthorId { get; set; }
+	public int AuthorId { get; set; }
+	public string? Name { get; set; }
+	public string Email { get; set; }
+
 	public required ICollection<Cheep> Cheeps { get; set; }
 }
