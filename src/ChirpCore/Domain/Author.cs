eecca227a8510faac3c193ee956cd21842656233
Maria Arnvig <arnv@itu.dk>
2024-11-15 14:07:31 +0100

    Refactor: remove migration and author change Co-authored-by: Jasmin <jsho@itu.dk>

--- src/ChirpCore/Domain/Author.cs
+++ src/ChirpCore/Domain/Author.cs
@@ -11,8 +11,7 @@ namespace ChirpCore.Domain
 		public int UserId { get; set; }
 		public required string Name { get; set; }
 
-		public override string? Email { get; set; }
-
+		public override required string? Email { get; set; }
 		public required ICollection<Cheep> Cheeps { get; set; }
 	}
 }
