2fca822ac29ef319322da2383d1e06ff4c5dc3fb
Maria Arnvig <arnv@itu.dk>
2024-11-15 14:55:50 +0100

    Refactor: remake database file Co-authored-by: Jasmin <jsho@itu.dk>

--- src/ChirpWeb/Program.cs
+++ src/ChirpWeb/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Load database connection via configuration, get string of database path from appsettings.json
-string? connectionString = builder.Configuration.GetConnectionString("ChirpDatabaseConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+string? connectionString = builder.Configuration.GetConnectionString("ChirpDatabaseConnection") ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
 
 //ChirpDBContext created with our database path - which is specified in appsettings.json
 builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
@@ -47,20 +47,23 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+// From ChatGPT to ensure the database is up to date with migrations when run 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
+    dbContext.Database.Migrate();
+}
+
 //Below 'using' block from Group 3. Seeds our database, and ensures that the database is created
+/*
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ChirpDBContext>();
     context.Database.EnsureCreated();
     DbInitializer.SeedDatabase(context);
-}
+}*/
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
-    dbContext.Database.Migrate();
-}
 
 app.Run();
 //public partial class Program { }
