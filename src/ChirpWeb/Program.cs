449d0b2b45b6ccb86a67caf039756fb2cc63dc8b
Maria Arnvig <arnv@itu.dk>
2024-11-19 15:33:12 +0100

    Please work commented migrate

--- src/ChirpWeb/Program.cs
+++ src/ChirpWeb/Program.cs
@@ -66,7 +66,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ChirpDBContext>();
     //context.Database.EnsureCreated();
-    await context.Database.MigrateAsync();
+    //await context.Database.MigrateAsync();
     DbInitializer.SeedDatabase(context);
 }
 
