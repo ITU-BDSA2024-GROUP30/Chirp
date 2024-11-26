a011dff485c91af05f831bc17671dc834d62da30
jsho <jsho@itu.dk>
2024-11-26 13:30:40 +0100

    Feat: Creating Cheeps method Moved from the other branch to this Does not work yet
    Co-authored-by: Jasmin <jsho@itu.dk>

--- src/ChirpInfrastructure/CheepRepository.cs
+++ src/ChirpInfrastructure/CheepRepository.cs
@@ -32,7 +32,77 @@ public class CheepRepository(ChirpDBContext context) : ICheepRepository
 
   }
   Above will be relevant later*/
-  public List<CheepDTO> ReadCheeps(int pageNumber)
+	/* Pseudo
+  function MakeCheep(newMessage)
+  cheepAuthor = GetAuthorById(newMessage.AuthorID) //Get the author synchronously
+
+  //Create a new Cheep object
+  message = New Cheep
+  message.CheepId = GenerateNextCheepId() //Increment the next Cheep ID
+  message.UserId = newMessage.AuthorID
+  message.Text = newMessage.Text
+  Message.Timestamp = CurrentDateTime() //Get the current date and the time
+  message.Author = cheepAuthor
+
+  //Add the cheep object to the database context (net yet persisted)
+  queryResult = AddCheepToDatabaseContext(message)
+
+  // Add the Cheep to the author's list of Cheeps
+  cheepAuthor.Cheeps.Add(message)
+
+  // Persist changes to the database
+  Await SaveChangesToDatabase()
+
+  // Log the Cheep details
+  Print("Store Cheep message = " + message.Text + " and AuthorId = " + message.Author.UserId)
+
+  // Return the ID of the newly created Cheep
+  Return queryResult.CheepId
+  End Function*/
+	public async Task<int> CreateCheep(CheepDTO newMessage)
+	{
+		//Get author sync
+		var cheepAuthor = await _context.Authors.FirstOrDefault(author => author.AuthorId == newMessage.AuthorID);
+
+		if (cheepAuthor == null)
+		{
+			throw new InvalidOperationException($"Author with ID {newMessage.AuthorID} not found.");
+		}
+
+		// Create a new Cheep object
+		var newCheep = new Cheep
+		{
+			CheepId = GenerateNextCheepId(),
+			AuthorId = newMessage.AuthorID,
+			Text = newMessage.Text,
+			TimeStamp = DateTime.UtcNow,
+			Author = cheepAuthor
+		};
+
+		// Add the Cheep object to the database context (not yet persisted)
+		var queryResult = await _context.Cheeps.AddAsync(newCheep);
+
+		// Add the Cheep to the author's list of Cheeps
+		cheepAuthor.Cheeps.Add(newCheep);
+
+		// Persist changes to the database
+		await _context.SaveChangesAsync();
+
+		// Log the Cheep details
+		Console.WriteLine($"Store Cheep message = {newCheep.Text} and AuthorId = {newCheep.AuthorId}");
+
+		// Return the ID of the newly created Cheep
+		return queryResult.Entity.CheepId;
+	}
+
+	private int GenerateNextCheepId()
+	{
+		// Example implementation: Fetch max CheepId from DB and increment
+		var maxId = _context.Cheeps.Max(c => (int?)c.CheepId) ?? 0;
+		return maxId + 1;
+	}
+
+	public List<CheepDTO> ReadCheeps(int pageNumber)
   {
 	int pageSize = 32;
 
