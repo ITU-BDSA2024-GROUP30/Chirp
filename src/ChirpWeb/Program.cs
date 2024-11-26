ca0709445f5c627decbed2cd6bd4b1112ac17cad
henri <hoes@itu.dk>
2024-11-26 19:46:32 +0100

    trying fixing

--- src/ChirpWeb/Program.cs
+++ src/ChirpWeb/Program.cs
@@ -11,65 +11,46 @@ using ChirpCore;
 using ChirpCore.Domain;
 using ChirpCore.DTOs;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-string path = Environment.GetEnvironmentVariable("chirpdbpath") ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
-// Load database connection via configuration, get string of database path from appsettings.json
-string connectionString = "Data Source=" + path;
-
-//ChirpDBContext created with our database path - which is specified in appsettings.json
-builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
-
-var dbcon = new SqliteConnection(connectionString);
-//await dbcon.OpenAsync();
-
-builder.Services.AddIdentity<Author, IdentityRole<int>>(options => options.SignIn.RequireConfirmedAccount = true)
-.AddDefaultUI()
-.AddDefaultTokenProviders()
-.AddEntityFrameworkStores<ChirpDBContext>();
-
-builder.Services.AddRazorPages();
-
-//Below 2 lines helps create Cheeps on the website and show Cheeps.
-builder.Services.AddScoped<ICheepService, CheepService>();
-builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) //removed !  might go back later
+public class Program
 {
-    // app.UseMigrationsEndPoint();
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		builder.Services.AddRazorPages();
+
+		//Below 2 lines helps create Cheeps on the website and show Cheeps.
+		builder.Services.AddScoped<ICheepService, CheepService>();
+		builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+
+		var app = builder.Build();
+
+
+		// app.UseMigrationsEndPoint();
+		string path = Environment.GetEnvironmentVariable("chirpdbpath") ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
+		// Load database connection via configuration, get string of database path from appsettings.json
+		string connectionString = "Data Source=" + path;
+
+		//ChirpDBContext created with our database path - which is specified in appsettings.json
+		//builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
+
+		using var dbcon = new SqliteConnection(connectionString);
+		dbcon.Open();
+
+
+		app.UseHttpsRedirection();
+
+		app.UseStaticFiles();
+		app.UseRouting();
+
+
+		app.UseAuthentication();
+		app.UseAuthorization();
+
+		app.MapRazorPages();
+
+
+		app.Run();
+
+	}
 }
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-}
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseRouting();
-
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-//Below 'using' block from Group 3. Seeds our database, and ensures that the database is created
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<ChirpDBContext>();
-    await context.Database.MigrateAsync();
-    context.Database.EnsureCreated();
-    DbInitializer.SeedDatabase(context);
-}
-
-app.Run();
-
-//partial class used for API tests in Chirp.ChirpWeb.Tests
-public partial class Program { }
