bfc639af64babf36e66cf769e8bd7e88a894131b
henri <hoes@itu.dk>
2024-12-04 20:33:45 +0100

    Updated Program to be ready for new path to database and minor fix in csproj in ChirpWeb
    Co-Authored: Henriette <hoes@itu.dk>
    Co-Authored: Maria <arnv@itu.dk>

--- src/ChirpWeb/Program.cs
+++ src/ChirpWeb/Program.cs
@@ -14,14 +14,30 @@ using ChirpCore.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Load database connection via configuration, get string of database path from appsettings.json
-//string connectionString = builder.Configuration.GetConnectionString("ChirpDatabaseConnection") ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
-string connectionString = "Data Source=:memory:";
+string connectionString = "";
+if (builder.Environment.IsDevelopment())
+{
+	//THIS is for local
+	connectionString = "Data Source=:memory:";
+	//this is showing in the terminal that it is local
+	Console.WriteLine("This is from local in builder environment development");
+
+	//This is an exampel for setting enviormentvariabel (of locally path for chirp.db) in the terminal Data Source=C:\tmp\ChirpData\chirp.db;
+	//miljøvariabel i kan være forskellige steder formateringer ifht forskellige terminaler (powershell, linux, mac osv) og computerer
+	//kig hvor jeres chirp.db, stifinder eller miljøvariabler på jeres computer
+
+}
+else
+{
+	//This is for Global
+	connectionString = builder.Configuration["CHIRPDBPATH"] ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
+	//this is showing in the terminal that it is local
+	Console.WriteLine("This is from gobal in builder in builder environment development (azure enviorment variabel)");
+
+}
 var dbcon = new SqliteConnection(connectionString);
 dbcon.Open();
 
-//string path = Environment.GetEnvironmentVariable("chirpdbpath") ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
-//string connectionString = "Data Source=" + path;
 
 //ChirpDBContext created with our database path - which is specified in appsettings.json
 builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(dbcon));
@@ -38,6 +54,10 @@ builder.Services.AddRazorPages();
 //Below 2 lines helps create Cheeps on the website and show Cheeps.
 builder.Services.AddScoped<ICheepService, CheepService>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+//for future migrations
+//builder.Services.AddScoped<IAuthorService, AuthorService>();
+//builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+
 
 var app = builder.Build();
 
