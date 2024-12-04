69a6be19aeed80cf18cf2e300bef8085b5147c7c
henri <hoes@itu.dk>
2024-12-04 17:11:41 +0100

    Deleted old chirpweb.db, we are using enviorment variabels from now on to tell where it is. Updated Program with locally and gobally enivorment variabel to connectionString for our database
    Co-Authored: Henriette <hoes@itu.dk>
    Co-Authored: Maria <arnv@itu.dk>

--- src/ChirpWeb/Program.cs
+++ src/ChirpWeb/Program.cs
@@ -13,18 +13,28 @@ using ChirpCore.DTOs;
 
 
 var builder = WebApplication.CreateBuilder(args);
-//Tror useless (old)
-//Load database connection via configuration, get string of database path from appsettings.json
-//string connectionString = "Data Source=" + path;
 
-//local
-string connectionString = "Data Source=:memory:";
+string connectionString = "";
+if (builder.Environment.IsDevelopment())
+{
+	//THIS is for local
+	connectionString = "Data Source=:memory:";
+	//this is showing in the terminal that it is local
+	Console.WriteLine("This is from local in builder environment development");
 
-//eller sætter denne som $env:CHIRPDBPATH="Data Source=C:\tmp\ChirpData\chirp.db" miljøvariabel i terminalen, forskelligt fra terminal til terminal
+	//This is an exampel for setting enviormentvariabel (of locally path for chirp.db) in the terminal Data Source=C:\tmp\ChirpData\chirp.db;
+	//miljøvariabel i kan være forskellige steder formateringer ifht forskellige terminaler (powershell, linux, mac osv) og computerer
+	//kig hvor jeres chirp.db, stifinder eller miljøvariabler på jeres computer
 
-//Global
-//string connectionString = builder.Configuration["CHIRPDBPATH"] ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
+}
+else
+{
+	//This is for Global
+	connectionString = builder.Configuration["CHIRPDBPATH"] ?? throw new InvalidOperationException("Connection string 'ChirpDatabaseConnection' not found.");
+	//this is showing in the terminal that it is local
+	Console.WriteLine("This is from gobal in builder in builder environment development (azure enviorment variabel)");
 
+}
 var dbcon = new SqliteConnection(connectionString);
 dbcon.Open();
 
