8c89f81259e6e8a3b14607a37f5d58331b2563d4
Maria Arnvig <arnv@itu.dk>
2024-12-08 22:03:59 +0100

    Refactor: removed email field when logging in Co-authored-by: Maria <arnv@itu.dk>

--- src/ChirpWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/ChirpWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,9 +73,9 @@ namespace ChirpWeb.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            //[Required]
+            //[EmailAddress]
+            //public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
