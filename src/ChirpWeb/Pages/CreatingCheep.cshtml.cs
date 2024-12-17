a76ee5d49e503c61269b39cfef47344b416edc53
tomat-suppe <slak@itu.dk>
2024-12-17 16:47:43 +0100

    updating to match main

--- src/ChirpWeb/Pages/CreatingCheep.cshtml.cs
+++ src/ChirpWeb/Pages/CreatingCheep.cshtml.cs
@@ -10,20 +10,14 @@ namespace ChirpWeb.Pages
 {
     public class CreateCheepModel : PageModel
     {
-        //private readonly ICheepRepository _repository;
-        //private readonly UserManager<Author> _userManager;
-        private readonly ICheepService _cheepService;
-
-        public CreateCheepModel(ICheepService cheepService){
-            _cheepService = cheepService;
-            CheepText = "";
-        }
-        /*public CreateCheepModel(ICheepRepository repository, UserManager<Author> userManager)
+        private readonly ICheepRepository _repository;
+        private readonly UserManager<Author> _userManager;
+        public CreateCheepModel(ICheepRepository repository, UserManager<Author> userManager)
         {
             _repository = repository;
             _userManager = userManager;
             CheepText = "";
-        }*/
+        }
         [BindProperty]
         [Required(ErrorMessage = "Please enter a message for your Cheep.")]
         [StringLength(160, ErrorMessage = "Cheep cannot exceed 160 characters.")]
@@ -36,8 +30,9 @@ namespace ChirpWeb.Pages
             {
                 return Page();
             }
+
             try
-            {   
+            {
                 //Console.WriteLine($"Attempting to create Cheep by userId: {userId}, userName: {userName}");
                 Author author = await _userManager.FindByNameAsync(User.Identity.Name);
                 
