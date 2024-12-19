0deb7fccd93138f932451230fb0ff2ea0b41230c
henri <hoes@itu.dk>
2024-12-19 14:00:37 +0100

    cleaning Program

--- src/ChirpWeb/Program.cs
+++ src/ChirpWeb/Program.cs
@@ -27,27 +27,9 @@ public partial class Program
 		var builder = WebApplication.CreateBuilder(args);
 
 		string connectionString = "";
-		//outcommented due to workflow on git
-		/*if (builder.Environment.IsDevelopment())
-		{
-			//THIS is for local
-			connectionString = "Data Source=:memory:";
-			//this is showing in the terminal that it is local
-			Console.WriteLine("This is from local in builder environment development");
 
-			//This is an exampel for setting enviormentvariabel (of locally path for chirp.db) in the terminal Data Source=C:/tmp/ChirpData/chirp.db;
-			//miljøvariabel i kan være forskellige steder formateringer ifht forskellige terminaler (powershell, linux, mac osv) og computerer
-			//kig hvor jeres chirp.db, stifinder eller miljøvariabler på jeres computer
-
-		}
-		else
-		{*/
-		//This is for Global
 		connectionString = builder.Configuration.GetConnectionString("CHIRPDBPATH") ?? throw new InvalidOperationException("Connection string not found.");
 
-		//Console.WriteLine("This is from global in builder in builder environment development (azure enviorment variabel)");
-
-		//}
 		var dbcon = new SqliteConnection(connectionString);
 		dbcon.Open();
 
