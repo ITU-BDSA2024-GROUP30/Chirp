aa73fed85cbab789d01e024031eee8628e6cfe8c
henri <hoes@itu.dk>
2024-12-19 01:59:39 +0100

    trying to make playwright Test setup and trying 1 test out with it
    Inspiration: Group 15
    And
    https://www.zenrows.com/blog/playwright-c-sharp#wait-for-element

---
+++ test/tests/PlayWrightTests/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace PlayWrightTests;
+
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+
+public class Class1
+{
+
+/*[Test]
+test10('has title', async({ page }) => {
+			await page.goto('https://playwright.dev/');
+
+			// Expect a title "to contain" a substring.
+			await expect(page).toHaveTitle(/ Playwright /);
+})*/
+
+[Test]
+public async Task HasTitleWFirstPlayWrightTest()
+{
+	await Page.GotoAsync("http://localhost:5273");
+
+	await Expect(Page).toHaveTitleAsync(new Regex("Chirp!"));
+
+}
+
+
+
+
+}
