efb9b15ec29c896635a245e39927d288fdd3dc0a
Green-Peber <114397502+HenrijetteOest@users.noreply.github.com>
2025-01-02 19:56:12 +0100

    Update CheepDTO.cs
    addition text and a spelling mistake

--- src/ChirpCore/DTOs/CheepDTO.cs
+++ src/ChirpCore/DTOs/CheepDTO.cs
@@ -2,12 +2,12 @@ namespace ChirpCore.DTOs
 {
 	/// <summary>
 	/// CheepDTO is used to transfer data of cheep(s) to our Chirp! Application.
-	/// Ensures that cheep(s) are unimmutable and sensitive data of our cheep is not exposed.
+	/// Ensures that cheep(s) are immutable and the sensitive data beloging of our cheep is not exposed.
 	/// </summary>
-	/// <param name="CheepId"></param>
-	/// <param name="UserName"></param>
-	/// <param name="Text"></param>
-	/// <param name="TimeStamp"></param>
+	/// <param name="CheepId">Primary key of our Cheep(s)</param>
+	/// <param name="UserName">Requried that a cheep has the username of the author creating the cheep(s).</param>
+	/// <param name="Text">Requried that a cheep has a text while the cheep(s) is created. </param>
+	/// <param name="TimeStamp">Requried that a cheep always has a timestamp of the creating of the cheep(s).</param>
 	/// <returns></returns>
 	public record CheepDTO(int CheepId, string UserName, string Text, string TimeStamp);
 }
